ab174669b14078d3b00e018d8b65349764a3a1aa
samridhip <pandeysamridhi1@gmail.com>
2024-01-06 23:40:29 +0530

    GUI Digital clock showing time and date using Java

---
+++ src/practice/Main.java
@@ -0,0 +1,11 @@
+import java.util.Calender;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+public class Clock extends Thread{
+    private static JLabel ct,data;
+    private static JFrame fr;
+    private Calendar cal;
+    
+}
---
+++ src/practice/MyWindow.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class MyWindow {
+    public static void main(String[]args){
+
+    }
+}
+
